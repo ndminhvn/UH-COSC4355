2114f7475508e2fc4878fdbc9da7456a2d11b1b4
Minh Nguyen <vn.ndminh@gmail.com>
2023-09-18 19:42:53 -0500

    setup storyboard for hw4

---
+++ Homework/HW4/Exercise4_Nguyen_Minh/Exercise4_Nguyen_Minh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise4_Nguyen_Minh
+//
+//  Created by Minh Nguyen on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
